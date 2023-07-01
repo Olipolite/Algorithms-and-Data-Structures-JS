30974e300361129d647e279a6b797deca4014a13
Olipolite <oliver.oquist3d@gmail.com>
2023-07-01 20:23:57 +0100

    Did quick sort and insertionSort, really cool stuff here

--- SortingAlgorithms.js
+++ SortingAlgorithms.js
@@ -22,3 +22,47 @@ bubbleSort(arr)
 console.log(arr) // [-6, -2, 4, 8, 20]
 
 //Big-O = O(n^2)
+
+// PROBLEM - Given an array of integers, sort the array.
+// j is the sorted part of the array. The while loop itirates over the sorted part of the array.
+
+const insertionSort = (array) => {
+    for (let i = 1; i < array.length; i++) {
+        let numberToInsert = array[i]
+        let j = i - 1
+        while(j >= 0 && array[j] > numberToInsert) {
+            array[j+1] = array[j]
+            j = j-1
+        }
+        array[j+1] = numberToInsert
+    }
+}
+
+const array = [8, 20, -2, 4, -6]
+insertionSort(array)
+console.log(array)
+
+// Big-O = O(n^2) 
+
+// PROBLEM - Given an array of integers, sort the array.
+//
+
+const quickSort = (quickArr) => {
+    if (quickArr.length < 2) {
+        return quickArr
+    }
+    let pivot = quickArr[quickArr.length - 1]
+    const leftArr = []
+    const rightArr = []
+    for (let i = 0; i < quickArr.length - 1; i++) {
+        if (quickArr[i] < pivot) {
+            leftArr.push(quickArr[i])
+        } else {
+            rightArr.push(quickArr[i])
+        }
+    }
+    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)]
+}
+
+const quickArr = [8, 20, -2, 4, -6]
+console.log(quickSort(quickArr))
